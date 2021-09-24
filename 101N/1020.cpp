f98268541e2de0c19e3c98bbe3ddd1d9637d7345
Jeonhyunjun <jhj091542@gmail.com>
2021-09-25 00:40:22 +0900

    수정 완료 (scanf,printf 함수 사용법 주석처리)

--- 101N/1020.cpp
+++ 101N/1020.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main()
 {
-    int a , b , c ;
-    char x;
-
-    cin >> a >> b >> c >> x ;
-
-   
-
-
-
+    int front , back;
+    scanf("%d-%d" , &front, &back );
+    printf("%06d%07d" , front, back);
 
     return 0;
 }
+
+
+
+// %d 는 int 타입의 정수형을 표현합니다.
+//변수 앞에 &를 붙이게 되면 이는 해당 변수의 주소를 가리킴
